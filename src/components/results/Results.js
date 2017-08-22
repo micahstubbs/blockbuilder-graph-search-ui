b225818192447718821d9ec5f112adca97309b92
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-21 21:59:29 -0700

    remove some logging and extraneous componentDidMount from Results

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react';
 import drawGraphVis from './drawGraphVis';
 
 export default class Results extends Component {
-  componentDidMount() {
-    if (this.props.results.length > 0) {
-      drawGraphVis(this.props.results);
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps from componentDidUpdate Results', prevProps);
-    console.log('this.props.results from componentDidUpdate Results', this.props.results);
+  componentDidUpdate() {
     if (this.props.results && typeof this.props.results.nodes !== 'undefined') {
       drawGraphVis(this.props.results);
     }
