4ded26dae69c33cacb9081a7324abc7e09b5a2bd
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-25 18:22:42 -0700

    disable eslint for libraries

--- src/lib/forceInABox.js
+++ src/lib/forceInABox.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import * as d3 from 'd3';
 
 export default function forceInABox(alpha) {
--- src/lib/jsLouvain.js
+++ src/lib/jsLouvain.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /*
 Author: Corneliu S. (github.com/upphiminn)
 
