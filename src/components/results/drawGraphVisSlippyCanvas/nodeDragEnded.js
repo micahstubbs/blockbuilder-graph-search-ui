60a706d40d50f7e410642ed17197d458f37fd118
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-09 15:28:23 -0700

    fix nodes in place after drag

--- src/components/results/drawGraphVisSlippyCanvas/nodeDragEnded.js
+++ src/components/results/drawGraphVisSlippyCanvas/nodeDragEnded.js
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 export default function dragended(props) {
   const { simulation } = props;
   if (!d3.event.active) simulation.alphaTarget(0);
-  d3.event.subject.fx = null;
-  d3.event.subject.fy = null;
+  // uncomment to unpin nodes
+  // d3.event.subject.fx = null;
+  // d3.event.subject.fy = null;
 }
