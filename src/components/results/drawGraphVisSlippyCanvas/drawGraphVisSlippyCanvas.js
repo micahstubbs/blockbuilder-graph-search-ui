cf2ba92584e6612d17f803c916ee76d5a5b34f08
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-08 18:51:04 -0700

    draw nodes on slippy canvas

--- src/components/results/drawGraphVisSlippyCanvas/drawGraphVisSlippyCanvas.js
+++ src/components/results/drawGraphVisSlippyCanvas/drawGraphVisSlippyCanvas.js
@@ -1,7 +1,10 @@
 import * as d3 from 'd3';
 import cloneDeep from 'lodash.clonedeep';
-import render from './render';
 
+import jLouvain from '../../../lib/jsLouvain';
+import forceInABox from '../../../lib/forceInABox';
+
+import render from './render';
 import cacheImages from '../cacheImages';
 
 export default function drawGraphVisSlippyCanvas(inputGraph) {
@@ -17,18 +20,124 @@ export default function drawGraphVisSlippyCanvas(inputGraph) {
   //
   //
   const imageCache = {};
+  const color = d3.scaleOrdinal().range(d3.schemeCategory20);
   let simulation;
-  const graph = cloneDeep(inputGraph);
 
+  // make a copy of inputGraph
+  // so that we have a pristine globalGraph
+  // to use the next time we call drawGraph
+  const graph = cloneDeep(inputGraph);
   cacheImages(graph, imageCache);
 
+  // Create the simulation with a small forceX and Y towards the center
+  simulation = d3
+    .forceSimulation()
+    .force('charge', d3.forceManyBody())
+    .force('x', d3.forceX(0).strength(0.003))
+    .force('y', d3.forceY(0).strength(0.003));
+
+  // clear the canvas
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  simulation.alphaTarget(0.2).restart();
+
+  //
+  // detect communities with jsLouvain
+  //
+  const nodeData = graph.nodes.map(function(d) {
+    return d.id;
+  });
+  const linkData = graph.links.map(function(d) {
+    return { source: d.source, target: d.target, weight: d.weight };
+  });
+
+  const community = jLouvain()
+    .nodes(nodeData)
+    .edges(linkData);
+  const result = community();
+
+  const nodeIndexHash = {};
+  graph.nodes.forEach(function(node, i) {
+    node.group = result[node.id];
+    nodeIndexHash[node.id] = i;
+  });
+
+  //
+  //
+  //
+  const users = d3
+    .nest()
+    .key(d => d.user)
+    .entries(graph.nodes)
+    .sort((a, b) => b.values.length - a.values.length);
+
+  color.domain(users.map(d => d.key));
+
+  //
+  // process links data to use simple node array index ids
+  // for source and target values
+  // to satisfy the assumption of the forceInABox layout
+  //
+  graph.links.forEach(link => {
+    // record the gistId
+    link.sourceGistId = link.source;
+    link.targetGistId = link.target;
+
+    // now use the node index
+    link.source = nodeIndexHash[link.source];
+    link.target = nodeIndexHash[link.target];
+  });
+
+  //
+  // Instantiate the grouping force
+  //
+  const groupingForce = forceInABox()
+    .strength(0.001) // Strength to foci
+    .template('force') // Either treemap or force
+    .groupBy('group') // Node attribute to group
+    .links(graph.links) // The graph links. Must be called after setting the grouping attribute
+    .size([width, height]); // Size of the chart
+
+  // Add your forceInABox to the simulation
+  simulation
+    .nodes(graph.nodes)
+    .force('group', groupingForce)
+    .force(
+      'link',
+      d3
+        .forceLink(graph.links)
+        .distance(50)
+        .strength(groupingForce.getLinkStrength) // default link force will try to join nodes in the same group stronger than if they are in different groups
+    );
+  // .on('tick', ticked);
+
+  for (var i = 0; i < 200; i++) {
+    simulation.tick();
+  }
+  simulation.stop();
+
+  // d3
+  //   .select(canvas)
+  //   .on('mousemove', mousemoved)
+  //   .on('click', clicked)
+  // .call(
+  //   d3
+  //     .drag()
+  //     .container(canvas)
+  //     .subject(dragsubject)
+  //     .on('start', dragstarted)
+  //     .on('drag', dragged)
+  //     .on('end', dragended)
+  // );
+
   //
   //
   //
 
-  const points = d3.range(2000).map(phyllotaxis(10));
+  // const points = d3.range(2000).map(phyllotaxis(10));
+  const points = graph.nodes;
 
-  console.log('canvas', canvas);
+  console.log('graph', graph);
 
   d3
     .select(canvas)
@@ -79,7 +188,7 @@ export default function drawGraphVisSlippyCanvas(inputGraph) {
     }
   }
 
-  function dragged() {
+  function dragged() { 
     renderProps = { context, width, height, transform, points, drawPoint };
     d3.event.subject[0] = transform.invertX(d3.event.x);
     d3.event.subject[1] = transform.invertY(d3.event.y);
@@ -87,8 +196,8 @@ export default function drawGraphVisSlippyCanvas(inputGraph) {
   }
 
   function drawPoint(point) {
-    context.moveTo(point[0] + radius, point[1]);
-    context.arc(point[0], point[1], radius, 0, 2 * Math.PI);
+    context.moveTo(point.x + radius, point.x);
+    context.arc(point.x, point.y, radius, 0, 2 * Math.PI);
   }
 
   function phyllotaxis(radius) {
