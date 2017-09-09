f5b51e98a77d63c7d99634d0c3e3132b145afc0b
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-08 21:24:36 -0700

    remove layout buttons

--- src/components/SearchControls.js
+++ src/components/SearchControls.js
@@ -1,39 +1,10 @@
 import React from 'react';
 
 export default class SearchControls extends React.Component {
-  gridClick() {
-    const { dispatchRenderGridLayout } = this.props;
-    dispatchRenderGridLayout();
-  }
-
-  boundedForceClick() {
-    const { dispatchRenderBoundedForceLayout } = this.props;
-    dispatchRenderBoundedForceLayout();
-  }
-
-  slippyCanvasClick() {
-    const { dispatchRenderSlippyCanvasLayout } = this.props;
-    dispatchRenderSlippyCanvasLayout();
-  }
 
   render() {
     return (
       <div className="layout">
-        <button id="grid-force-button" onClick={this.gridClick.bind(this)}>
-          grid force
-        </button>
-        <button
-          id="bounded-force-button"
-          onClick={this.boundedForceClick.bind(this)}
-        >
-          bounded force
-        </button>
-        <button
-          id="slippy-canvas-button"
-          onClick={this.slippyCanvasClick.bind(this)}
-        >
-          slippy canvas
-        </button>
       </div>
     );
   }
