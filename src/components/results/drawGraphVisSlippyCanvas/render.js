e3bd84015a28a4222d1d740f4dae51bbb417e8bd
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-11 17:25:01 -0700

    get links to render too

--- src/components/results/drawGraphVisSlippyCanvas/render.js
+++ src/components/results/drawGraphVisSlippyCanvas/render.js
@@ -1,23 +1,28 @@
-import drawLink from './drawLink';
+// import drawLink from './drawLink';
 import drawNode from './drawNode';
 
 export default function render(props) {
   const { context, width, height, graph, imageCache, rects, radius } = props;
-
-  context.save();
   context.clearRect(0, 0, width, height);
   // draw the invisible background
   rects.forEach(rect => {
     context.clearRect(0, 0, width, height);
 
     // draw the links
-    context.strokeStyle = '#aaa';
-    context.lineWidth = 1;
-    context.beginPath();
-
-    const drawLinkProps = { graph, context, rect };
-    graph.links.forEach(drawLink.bind(this, drawLinkProps));
-    context.stroke();
+      context.strokeStyle = '#aaa';
+      context.lineWidth = 1;
+      context.beginPath();
+      graph.links.forEach(link => {
+        context.moveTo(
+          graph.nodes[link.source.index].x + rect.x,
+          graph.nodes[link.source.index].y + rect.y
+        );
+        context.lineTo(
+          graph.nodes[link.target.index].x + rect.x,
+          graph.nodes[link.target.index].y + rect.y
+        );
+      });
+      context.stroke();;
 
     // draw the nodes
     context.beginPath();
@@ -30,6 +35,4 @@ export default function render(props) {
     graph.nodes.forEach(drawNode.bind(this, drawNodeProps));
     context.fill();
   });
-
-  context.restore();
 }
