ac08ba25d0afd43b5d0d99c06476538a62bdd17e
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-15 14:30:57 -0700

    ensure that a block is opened in a new window only if the data under the mouse has an id property. fix #43

--- src/components/results/drawGraphVisSlippyCanvas/onClick.js
+++ src/components/results/drawGraphVisSlippyCanvas/onClick.js
@@ -2,7 +2,7 @@ export default function onClick(props) {
   const { currentSubject } = props;
   const d = currentSubject[0];
 
-  if (typeof d !== 'undefined') {
+  if (typeof d.id !== 'undefined') {
     const blockUrl = `http://bl.ocks.org/${d.user ? `${d.user}/` : ''}${d.id}`;
     window.open(blockUrl);
   }
