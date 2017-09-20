6ae8fe83290803cc6000aa1696c29ea690f869d3
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-19 17:08:21 -0700

    get triangle to show on the page

--- src/components/results/drawGraphVisSlippyCanvas/render.js
+++ src/components/results/drawGraphVisSlippyCanvas/render.js
@@ -9,20 +9,27 @@ export default function render(props) {
     context.clearRect(0, 0, width, height);
 
     // draw the links
-      context.strokeStyle = '#aaa';
-      context.lineWidth = 1;
-      context.beginPath();
-      graph.links.forEach(link => {
-        context.moveTo(
-          graph.nodes[link.source.index].x + rect.x,
-          graph.nodes[link.source.index].y + rect.y
-        );
-        context.lineTo(
-          graph.nodes[link.target.index].x + rect.x,
-          graph.nodes[link.target.index].y + rect.y
-        );
-      });
-      context.stroke();;
+    context.strokeStyle = '#aaa'; // link gray
+    context.lineWidth = 1;
+    context.beginPath();
+    graph.links.forEach(link => {
+      context.moveTo(
+        graph.nodes[link.source.index].x + rect.x,
+        graph.nodes[link.source.index].y + rect.y
+      );
+      context.lineTo(
+        graph.nodes[link.target.index].x + rect.x,
+        graph.nodes[link.target.index].y + rect.y
+      );
+    });
+    context.stroke();
+
+    const path = new Path2D();
+    path.moveTo(100 / 2 + 50, 100 / 2);
+    path.lineTo(100 / 2, 100 / 2 - 50);
+    path.lineTo(100 / 2 - 50, 100 / 2);
+    context.fillStyle = '#aaa'; // link gray
+    context.fill(path);
 
     // draw the nodes
     context.beginPath();
