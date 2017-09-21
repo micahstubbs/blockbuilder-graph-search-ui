57f9d852e7ce1e3bc5863a6d2c6681ff4b5efa67
Micah Stubbs <micah.stubbs@gmail.com>
2017-09-21 14:43:38 -0700

    show text for query results fix #55

--- src/components/SearchControls.js
+++ src/components/SearchControls.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 export default class SearchControls extends React.Component {
-
   render() {
+    const { results } = this.props;
+    let resultsIcon = '';
+    let resultsText = '';
+    if (
+      typeof results !== 'undefined' &&
+      typeof results.nodes !== 'undefined'
+    ) {
+      const { nodes, links } = results;
+
+      if (nodes.length > 0) {
+        resultsIcon = `🔎`
+        resultsText = `found ${nodes.length} nodes and ${links.length} links`;
+      }
+    }
     return (
-      <div className="layout">
+      <div>
+        <div className="results-icon">{resultsIcon}</div>
+        <div className="results-text">{resultsText}</div>
       </div>
     );
   }
